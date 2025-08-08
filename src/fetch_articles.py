cbf8c527d704778b3116217c3fcdc3ca239b308f
Jen Pallister <pallisterjen@gmail.com>
2025-08-08 12:50:23 -0700

    Create fetch_articles.py

---
+++ src/fetch_articles.py
@@ -0,0 +1,28 @@
+# fetch_articles.py
+
+import feedparser
+from newsapi import NewsApiClient
+
+# Set up News API
+newsapi = NewsApiClient(api_key="af31da346f66425180945ae9602532b7")
+
+# RSS Feed URLs
+rss_urls = [
+    "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
+    "https://feeds.bbci.co.uk/news/business/rss.xml",
+    "https://www.reuters.com/rssfeeds/technology/",
+    "https://www.theguardian.com/world/rss",
+    "https://www.cnbc.com/id/10000664/device/rss/rss.html"
+]
+
+def get_rss_articles(rss_urls, max_articles=10):
+    articles = []
+    for url in rss_urls:
+        feed = feedparser.parse(url)
+        for entry in feed.entries[:max_articles]:
+            articles.append({"title": entry.title, "summary": entry.summary, "link": entry.link})  # Added link
+    return articles
+
+def get_newsapi_articles(max_articles=10):
+    top_headlines = newsapi.get_top_headlines(language='en', page_size=max_articles)
+    return [{"title": a["title"], "description": a["description"], "link": a["url"]} for a in top_headlines["articles"]]  # Added link
